5a94a4438dcd7444bc4ff998c20d338f50f38916
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-03-31 15:20:41 +0900

    JAVA수업자료 업로드

---
+++ WorkSpace/02.Valiable/src/pack01_Valiable/Ex03_Valiable.java
@@ -0,0 +1,17 @@
+package pack01_Valiable;
+
+public class Ex03_Valiable {
+	public static void main(String[] args) {
+//		int value = ;
+//		int result = value + 10;
+		int value = 30;
+		int result = value + 10;
+		System.out.println(result);
+		//숫자형 변수들은 사칙연산이 가능하다.
+		//재할당 (변수를 계속 만드는 게 아니라 값을 다시 줘서 바꿈)
+		result = 1*0+(15/5-1);
+		System.out.println(result);
+		result = value ;//같은 타입의 변수는 값을 담을 수 있다.
+	}
+
+}
---
+++ WorkSpace/02.Valiable/src/pack07_escape/Test01_Escape.java
@@ -0,0 +1,15 @@
+package pack07_escape;
+
+public class Test01_Escape {
+	public static void main(String[] args) {
+		System.out.println("\\\t/\\");
+		System.out.println(" )\t( ')");
+		System.out.println(" (\t/)");
+		System.out.println(" \\   (__)|");
+		System.out.println("\\\t/\\");
+		System.out.println(" )\t( ')");
+		System.out.println(" (\t/)");
+		System.out.println(" \\   (__)|");
+	}
+
+}
