16487ec7257b2b0b600289ccd3b128e2af74d85f
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-04-07 13:05:09 +0900

    15
    15

--- WorkSpace/999.Test/src/Test_01.java
+++
@@ -1,9 +0,0 @@
-
-public class Test_01 {
-	public static void main(String[] args) {
-		// ; <= 세미콜론, : <= 콜론
-		// 자바 프로그램은 ;(세미콜론) 기준으로 명령의 끝을 구분한다
-		System.out.println();
-		int num;
-	}
-}
--- WorkSpace/HelloWorldSBN/src/HelloWorldSBN.java
+++
@@ -1,6 +0,0 @@
-
-public class HelloWorldSBN {
-public static void main(String[] args) {
-	System.out.println("sbn");
-}
-}
